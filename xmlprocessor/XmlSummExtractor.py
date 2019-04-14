cf5724387bc8a10d439e10d0c55ce1334dc2f0cf
SAMEED\sameed <sameedtariq@gmail.com>
2019-04-14 11:06:04 +0200

    1. Seperate CrashID, Number of Vehicles and Summary from XML file 2. Store the requirement in seperate json file with crash ID.

---
+++ xmlprocessor/XmlSummExtractor.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+import json
+import glob
+
+xmlFiles = glob.glob("../resources/nhtsa/*.xml")
+summFolderPath = "../resources/summary/"
+
+for xmlFile in xmlFiles:
+    tree = ET.parse(xmlFile);
+    root = tree.getroot();
+
+    print(root.attrib)
+    # Empty dict
+    dict = {}
+    # Fill in the entries one by one
+    for name, value in root.attrib.items():
+        if name == 'CaseID':
+            dict[name] = root.attrib[name]
+            print(root.attrib[name])
+
+        if name == 'NumOfVehicle':
+            dict[name] = root.attrib[name]
+            print(root.attrib[name])
+
+    for child in root:
+        crashSummary = child.find('Crash/CRASH/XML_CASESUMMARY/SUMMARY')
+        dict[crashSummary.tag] = crashSummary.text
+    #    print(crashSummary.text)
+    summJson = json.dumps(dict)
+    print(summJson)
+
+    with open(summFolderPath+dict.get('CaseID')+'.json', 'w') as outfile:
+        json.dump(dict, outfile)
+
+
