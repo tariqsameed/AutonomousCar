a303005a8a3d0739c518b824ca96038ba837eb58
sameed tariq <sameedtariq@gmail.com>
2019-05-05 15:47:13 +0200

    1. Method to Call opnie service in Java to extrax subject verb object triples. 2. read the result (json) from opnie

--- summanalyzer/summaryanalyzer.py
+++ summanalyzer/summaryanalyzer.py
@@ -47,9 +47,16 @@ for key, value in summDict.items():
 
     # Extract V1 information
     #https://www.linkedin.com/pulse/triplets-concept-extraction-from-english-sentence-deep-swamynathan
+    # https://pypi.org/project/regex4dummies/
+    # https://playwithml.wordpress.com/2016/06/15/extracting-relations-or-subject-predicate-object-triples/
+    # https://stackoverflow.com/questions/2705888/rdf-representation-of-sentences/2706567#2706567
+    # https://stackoverflow.com/questions/29049974/typed-dependency-parsing-in-nltk-python
+    #https://www.analyticsvidhya.com/blog/2019/02/stanfordnlp-nlp-library-python/
     #https://pypi.org/project/stanfordnlp/
     # https://stackoverflow.com/questions/39763091/how-to-extract-subjects-in-a-sentence-and-their-respective-dependent-phrases
     # https://github.com/krzysiekfonal/textpipeliner
+
+    # https://stanfordnlp.github.io/stanfordnlp/
 
     # Extract V2 information
 
--- summanalyzer/triplesextractor.py
+++ summanalyzer/triplesextractor.py
@@ -1,8 +1,18 @@
 import spacy
 import textacy
+import requests
 
-nlp = spacy.load("en_core_web_sm")
-text = nlp(u'	This crash occurred on a two-way, not physically divided roadway, in the middle of a "T" intersection. V1 was traveling west, turning right. The front of V1 contacted the front of V2 as V2 was heading south. V1 had to be towed from the scene, but no occupants were transported. The road conditions at the time of the accident were wet, and it was raining.')
-text_ext = textacy.extract.subject_verb_object_triples(text)
-print(list(text_ext))
+def gettriplespacy(text):
+    nlp = spacy.load("en_core_web_sm")
+    text = nlp(text)
+    text_ext = textacy.extract.subject_verb_object_triples(text)
+    print(list(text_ext))
 
+
+def gettripleopie(sentext):
+    payload = {'text': sentext}
+    r = requests.get("http://localhost:8080/opie", params=payload)
+    resp = r.text
+    if resp['length'] > 0:
+    for val in enumerate(resp['spoList']):
+        print(val[1]['subject'],val[1]['predicate'],val[1]['object'])
