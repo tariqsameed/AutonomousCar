621ffa53d76a7ca775090eb4078d20b428280d6a
SAMEED\sameed <sameedtariq@gmail.com>
2019-07-29 03:12:58 +0200

    1. Fix the width of each lane in lane marking.

--- osm_beamng_scenario/osm_beamng_road_lane_creation.py
+++ osm_beamng_scenario/osm_beamng_road_lane_creation.py
@@ -1,9 +1,5 @@
 import math
-from geopy.distance import great_circle
 import pickle
-import networkx as nx
-import matplotlib.pyplot as plt
-import sys
 from beamngpy import BeamNGpy, Scenario, Road, Vehicle, setup_logging
 
 filename = 'passau'
@@ -22,17 +18,23 @@ beamng_dict = pickle.load(open(map_beamng_serialize, "rb"))
 print("BeamNG Nodes Loaded")
 
 def getRoadPolyLineLaneMarking(point1,point2, width):
-    print("Road Lanes")
 #https://stackoverflow.com/questions/47040213/find-perpendicular-line-using-points-on-that-line
 
-    print(point1,point2, width)
+    #print(point1,point2, width)
+    lane_adjustment = 0
+
+    if width < 0:
+        lane_adjustment = -0.2
+    elif width > 0:
+        lane_adjustment = 0.2
+
 
     dx = float(point2[0] - point1[0])
     dy = float(point2[1] - point1[1])
 
     L = float(math.sqrt(float(float(dx * dx) + float(dy * dy))))
     U = (float(-dy / L), float(dx / L))
-    F = float(float(width) - 0.05)
+    F = float(float(width / 2) - lane_adjustment)
 
 # Point on one side
     x1p = float(point1[0] + U[0] * F)
@@ -50,21 +52,21 @@ def getRoadPolyLineLaneMarking(point1,point2, width):
 
 
 def getLanesAndWidth(width, lanes):
-    print("Lanes and Width")
+    #print("Lanes and Width")
 # https://stackoverflow.com/questions/9926446/how-to-check-whether-a-strvariable-is-empty-or-not
 
     total_lanes = lanes
     total_width = width
 
     if width and lanes:
-        print("Width and Lanes")
+        #print("Width and Lanes")
         # used lane width data to compute each lane width
         total_lanes = lanes
         total_width = width
 
 
     if width and not lanes:
-        print("width only")
+        #print("width only")
         # lanes based on the formula.
         number_of_lanes = float(width / 3.7)
         total_lanes = round(number_of_lanes)
@@ -72,7 +74,7 @@ def getLanesAndWidth(width, lanes):
 
 
     if lanes and not width:
-        print("lanes only")
+        #print("lanes only")
         # standard lane width 4
         total_lanes = lanes
         total_width = lanes * 4
@@ -85,7 +87,7 @@ def getLanesAndWidth(width, lanes):
 
 
 def getRoadLaneMarking(point1,point2, width, lanes):
-    print("Road Lane marking")
+    #print("Road Lane marking")
     lanesAndWidth = getLanesAndWidth(width,lanes)
     center = float(width/2)
     laneWidth = float(lanesAndWidth[1] / lanesAndWidth[0])
@@ -195,7 +197,7 @@ def createBeamNGRoads():
         # for loop iterate to put polylines.
 
         for polyline in lane_marking_points:
-            print(polyline)
+            #print(polyline)
             road_d = Road('track_editor_C_border', looped=False)
 
             nodes0 = [
